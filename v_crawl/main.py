818798a4506132ce6cac9232acd83c5f1509696a
BreakBB <b.boeing@online.de>
2018-11-29 13:11:03 +0100

    Refactor main to start the crawling in scrapy way.

--- v_crawl/main.py
+++ v_crawl/main.py
@@ -1,2 +1,12 @@
-from scrapy import cmdline
-cmdline.execute("scrapy runspider spiders/amazon_de_spider.py".split())
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+from v_crawl.spiders.amazon_de_spider import AmazonDeSpider
+
+
+print("Starting the run of %s" % AmazonDeSpider.name)
+print("Timeout is set to %i" % AmazonDeSpider.spider_timeout)
+
+process = CrawlerProcess(get_project_settings())
+process.crawl(AmazonDeSpider)
+process.start()
